96783e5fc370ee6d573e2457c39db1020786b722
MD MUTASIM BILLAH <70066678+sshatil@users.noreply.github.com>
2026-06-14 19:30:05 +0800

    chore: upgrade Expo SDK & modify prayer time utils

--- utils/prayer-time.ts
+++ utils/prayer-time.ts
@@ -1,15 +1,15 @@
 export const getPrayerIcon = (prayerKey: string) => {
   switch (prayerKey) {
     case 'Fajr':
-      return 'moon-outline';
+      return 'cloudy-night-outline';
     case 'Dhuhr':
       return 'sunny-outline';
     case 'Asr':
       return 'partly-sunny-outline';
     case 'Maghrib':
-      return 'partly-sunny-outline';
+      return 'partly-sunny';
     case 'Isha':
-      return 'cloudy-night-outline';
+      return 'moon-outline';
     default:
       return 'time-outline';
   }
